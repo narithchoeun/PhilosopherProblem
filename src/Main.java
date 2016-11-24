a880a7369a3a085b4f57cd8f9b4707f847f4954f
Narith Choeun <narithchoeun@hotmail.com>
2016-11-24 13:09:56 -0800

    added comments

--- src/Main.java
+++ src/Main.java
@@ -13,25 +13,28 @@ How well does your solution scale? Do you observe any bottleneck?
 Submit your program, cover sheet & a summary/discussion of the 4 experiments.
 */
 import java.util.concurrent.locks.*;
+
 public class Main {
   private static int WAITING = 0, EATING = 1, THINKING = 2;
-  private static final int NUM_PHILS = 5;
+  private static final int NUM_PHILS = 10;
   private static Lock lock = new ReentrantLock();
-  private static Condition phil [] = new Condition[NUM_PHILS];
+  private static Condition philosophers [] = new Condition[NUM_PHILS];
   private static int states [] = new int[NUM_PHILS];
 
-  public static void init ( ) {
+  // init all philosophers to THINKING
+  public static void init () {
     for (int k = 0; k < NUM_PHILS; k++) {
-      phil[k] = lock.newCondition();
+      philosophers[k] = lock.newCondition();
       states[k] = THINKING;
     }
   }
 
+  // main
   public static void main(String[] args) {
     init();
     Philosopher p[] = new Philosopher[NUM_PHILS];
     for (int k = 0; k < p.length; k++) {
-      p[k] = new Philosopher(lock, phil, states, NUM_PHILS);
+      p[k] = new Philosopher(lock, philosophers, states, NUM_PHILS);
       p[k].start();
     }
   }
--- src/Philosopher.java
+++ src/Philosopher.java
@@ -1,36 +1,47 @@
 import java.util.concurrent.locks.*;
+import java.util.Random;
 
 public class Philosopher extends Thread {
   private static int WAITING = 0, EATING = 1, THINKING = 2;
   private Lock lock;
-  private Condition phil [];
-  private int states [];
+  private Condition philosophers [];
+  private int states []; //implicitly describes forks
   private int NUM_PHILS;
   private int id;
-  private final int TURNS = 20;
+  private final int TURNS = 2;
 
-  // con
+  private int maxSleepTime = 100;
+  private Random rand = new Random();;
+  private long execTime = 0, waitTime = 0;
+  private int stallCtr = 0;
+
+  // constructor
   public Philosopher (Lock l, Condition p[], int st[], int num) {
     lock = l;
-    phil = p;
+    philosophers = p;
     states = st;
     NUM_PHILS = num;
   }
 
+  // as soon as thread starts, assign its id, and takeSticks()/putSticks() for TURNS
   public void run () {
     id = ThreadID.get();
+
     for (int k = 0; k < TURNS; k++) {
       try {
-        sleep(100);
-      } catch (Exception ex) { /* lazy */}
+        sleep(randomTime());
+      } catch (Exception ex) { /* lazy */ }
+
       takeSticks(id);
 
       try {
-        sleep(20);
+        sleep(randomTime());
       } catch (Exception ex) { }
+
       putSticks(id);
+
+      output();
     }
-    output();
   }
 
   // if the left and right chopstick is free for the current philosopher, they can enter the EATING state
@@ -42,8 +53,8 @@ public class Philosopher extends Thread {
         states[id] = EATING;
       else {
         states[id] = WAITING;
-        //philosopher waits until signalled or interrupted
-        phil[id].await();
+        //philosopher waits until signaled or interrupted
+        philosophers[id].await();
       }
     } catch(InterruptedException e){
       System.exit(-1);
@@ -52,42 +63,55 @@ public class Philosopher extends Thread {
     }
   }
 
+  // print the states of the philosophers
   public void output() {
     lock.lock();
     for (int k = 0; k < states.length; k++)
-      System.out.print(states[k]+",");
+      System.out.print(states[k] + ",");
     lock.unlock();
     System.out.println();
     System.out.println();
   }
 
+  // philosopher is finished eating, notify the left & right philosopher if they are waiting and have a 2nd utensil available
   public void putSticks (int id) {
     lock.lock();
     try {
       states[id] = THINKING;
+      //if the left philosopher is waiting to eat and the left of that philosopher is not eating,
+      //signal that person that the stick is now available to use and begin eating
       if (states[leftof(id)]==WAITING && states[leftof(leftof(id))]!=EATING) {
-        phil[leftof(id)].signal();
+        philosophers[leftof(id)].signal();
         states[leftof(id)] = EATING;
       }
+
+      //same for right philosopher
       if (states[rightof(id)] == WAITING && states[rightof(rightof(id))] != EATING) {
-        phil[rightof(id)].signal(); states[rightof(id)] = EATING;
+        philosophers[rightof(id)].signal();
+        states[rightof(id)] = EATING;
       }
+
     } finally {
       lock.unlock();
     }
   }
 
   private int leftof (int id) { // clockwise
-    int retval = id-1;
-    if (retval < 0) // not valid id
-      retval = NUM_PHILS-1;
+    int retval = id - 1;
+    if (retval < 0) // not valid id, cycle through again
+      retval = NUM_PHILS - 1;
     return retval;
   }
 
   private int rightof (int id) {
-    int retval = id+1;
-    if (retval == NUM_PHILS) // not valid id
-      retval=0;
+    int retval = id + 1;
+    if (retval == NUM_PHILS) // not valid id, recycle
+      retval = 0;
     return retval;
+  }
+
+  //return a random int to be used for the sleep function
+  private int randomTime() {
+    return rand.nextInt(maxSleepTime);
   }
 }
